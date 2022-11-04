f3f80734e472f0f30ff98a8aab59c0eb563c26bf
LucaBonifazio <luca.bonifazio13@gmail.com>
2022-11-04 12:23:09 +0100

    add mine

--- js/script.js
+++ js/script.js
@@ -1,63 +1,75 @@
-// Consegna
-
-// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
-// In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
-// La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
-// BONUS:
-// Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
-// - difficoltà 1 ⇒ 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
-// - difficoltà 2 ⇒ 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
-// - difficoltà 3 ⇒ 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
-
 const eleGrid = document.querySelector('.grid');
 const eleWelcomeText = document.querySelector('.welcome-text')
 const eleMode = document.querySelector('#mode');
 const eleBtn = document.querySelector('.btn');
-const arrRandoms = [];
+let arrMines;
+let score;
 
 eleBtn.addEventListener('click', function () {
-    
-    eleGrid.replaceChildren();
-    eleGrid.classList.remove('hidden');
-    eleWelcomeText.classList.add('hidden');
+	score = 0;
+	const nCells = parseInt(eleMode.value);
+	const nMines = 16;
+	maxScore = nCells - nMines;
+	arrMines = generateMines(nMines, 1, nCells);
+	
+	eleGrid.innerHTML = '';
 
-    const nCells = parseInt(eleMode.value);
-    const sideSquare = Math.sqrt(nCells);
-    eleGrid.style.setProperty('--sideSquare', sideSquare);
+	eleGrid.classList.remove('hidden');
+	eleWelcomeText.classList.add('hidden');
 
-    for (let i = 1; i <= nCells; i++) {
-        const eleCell = document.createElement('div');
-        eleCell.classList.add('cell');
-        eleCell.innerHTML = i;
-        eleGrid.append(eleCell);
+	const sideSquare = Math.sqrt(nCells);
+	eleGrid.style.setProperty('--sideSquare', sideSquare);
 
-        eleCell.addEventListener('click', toggleCell);
-    }
+	for (let i = 1; i <= nCells; i++) {
+		const eleCell = document.createElement('div');
+		eleCell.classList.add('cell');
+		eleCell.innerHTML = i;
+		eleGrid.append(eleCell);
+
+		eleCell.addEventListener('click', toggleCell);
+	}
 });
 
-for (let i = 1; i <= 16; i++) {
-    let randomNumberMine;
-    do {
-        randomNumberMine = getRandomInteger(1, 100); 
-    } while (arrRandoms.includes(randomNumberMine))
-    arrRandoms.push(randomNumberMine);
+function toggleCell() {
+	const cellNumber = parseInt(this.innerHTML);
+
+	if (arrMines.includes(cellNumber)) { 
+		this.classList.add('mine');
+		disableAllCells(true);
+		alert('Il tuo punteggio e: ' + score);
+	} else {
+		this.removeEventListener('click', toggleCell); 
+		score++;
+		this.classList.add('no-mine');
+		if (score == maxScore) {
+			disableAllCells(false);
+			alert('Complimenti hai vinto! Il tuo punteggio e: ' + score);
+		}
+	}
 }
 
-console.log(arrRandoms);
-
-function toggleCell() {
-	this.classList.toggle('active');
+function generateMines(nMines, min, max) {
+	const arrRandoms = [];
+	for (let i = 0; i < nMines; i++) {
+		do {
+			randomNumber = getRandomInteger(min, max);
+		} while (arrRandoms.includes(randomNumber))
+		arrRandoms.push(randomNumber);
+	}
+	return arrRandoms;
 }
 
 function getRandomInteger(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-// function isMine(number) {
-// 	if (number % 2) {
-// 		return false;
-// 	} else {
-// 		return true;
-// 	}
-// }
+function disableAllCells(showMines) {
+	const listCells = eleGrid.querySelectorAll('.cell');
+	for (let i = 0; i < listCells.length; i++) {
+		const cellNumber = parseInt(listCells[i].innerHTML);
+		if (showMines && arrMines.includes(cellNumber)) {
+			listCells[i].classList.add('mine');
+		}
+		listCells[i].removeEventListener('click', toggleCell);
+	}
+}
